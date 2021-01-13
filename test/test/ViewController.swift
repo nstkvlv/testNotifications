01be1ed0b3715e3d2a4579c67b91759f79fa7d8d
Anastasia Kovaleva <2878678@gmail.com>
2021-01-13 11:14:57 +0300

    create project

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Анастасия Ковалева on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
